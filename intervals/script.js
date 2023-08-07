0acc32d98423492b73308002825d3a214d6aecb9
ascronald <roszeto@stu.allstarcode.org>
2023-08-06 21:38:48 -0400

    Final commit

--- intervals/script.js
+++ intervals/script.js
@@ -3,3 +3,11 @@ let time = 0;
 // reference to the <span> containing the number
 let counterElement = document.getElementById("time");
 
+
+
+function updateCounter() {
+    time++;
+    counterElement.textContent = time;
+  }
+
+setInterval(updateCounter, 1000);
--- race-to-the-finish/sketch.js
+++ race-to-the-finish/sketch.js
@@ -24,29 +24,30 @@ function draw() {
     // show scores
     textSize(60);
     fill(255, 0, 0);
-    text(redScore, 800, 100);
+    text(redScore, 650, 100);
     fill(0, 0, 255);
-    text(blueScore, 800, 225);
+    text(blueScore, 650, 225);
 
     // show finish line
-    line(600, 0 600, 300);
+    line(600, 0 , 600, 300);
 
     // red ball
     fill(255, 0, 0);
-    ellipse(50, 50, redXPos, redYPos);
+    ellipse(redXPos, redYPos, 50, 50);
     
     // blue ball
     fill(0, 0, 255);
-    ellipse(50, 50, blueXPos, blueYPos);
+    ellipse(blueXPos, blueYPos, 50, 50);
 
     // crossed finish line
-    if (redXPos < 575 || blueXPos < 575) {
-        if (redXPos > blueXPos) {
+    if (redXPos > 575 || blueXPos > 575) {
+        if (redXPos < blueXPos) {
             // blue wins
-            redScore += 1;
-        } else if (blueXPos > redXPos) {
+            blueScore += 1;
+        } 
+            else if (blueXPos < redXPos) {
             // red wins
-            blueScore = 1;
+            redScore += 1;
         }
         // reset positions
         redXPos = 25;
